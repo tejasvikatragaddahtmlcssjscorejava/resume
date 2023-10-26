6e0740988e69d292c29506ab48e045a30c229cc3
tejasvikatragaddahtmlcssjscorejava <144216237+tejasvikatragaddahtmlcssjscorejava@users.noreply.github.com>
2023-10-26 12:54:08 +0530

    git commit

---
+++ Untitled-1.js
@@ -0,0 +1,22 @@
+//basic function
+//what is the use of function--perofrm a specific task unline other program
+//what is use of function
+//function need to call-by some object or class
+//if a function is passed as a parameter to anothe function from another function its known as call back function
+//inner function remembers outer function values even after outher function execution ended is known as closures-memorization
+//fetch promise 
+DOM manipulation
+
+function add (a.b)
+{
+    console.log("addition"${a=b});
+}
+call function sub (c)
+}
+window object 
+//function mul()
+//{
+//comsole.log
+('multiplication')
+//}
+const k =mul(2,3) 
---
+++ movies.js
@@ -0,0 +1,24 @@
+//async function -return promises
+const button=document.getElementById('btn')
+button.addEventListener('click',function())
+
+
+
+
+
+
+//
+async function search movies (st)
+{
+    const api key ="399a3064"
+    const apiurl= http://www.omdbapi.com//s=${st}
+    try
+}
+debbuger;
+fetch(apiurl) // what fetcj methord will return
+//.then(function(res)){return.resjson()}{apikey}
+try
+{
+const reponce=await
+fetch api url
+
